c04340d07a4bca66bc3a213cc70d9309844d16b3
Mohit Kumar <mohitkumar.7@outlook.com>
2020-04-26 07:44:56 +0530

    chore: style insert image page

--- app/components/InsertMedia/index.js
+++ app/components/InsertMedia/index.js
@@ -7,11 +7,10 @@
 import React, { memo, useState } from 'react';
 // import PropTypes from 'prop-types';
 
-import uploadIcon from 'images/icons/upload.svg'
+import uploadIcon from 'images/icons/upload.svg';
 import Styled from './style';
 
 function InsertMedia() {
-
   const [previewURL, setPreviewURL] = useState('');
 
   function handleImageChange(event) {
@@ -19,8 +18,7 @@ function InsertMedia() {
       target: { files },
     } = event;
     const imageURL = URL.createObjectURL(files[0]);
-    console.log(imageURL)
-    setPreviewURL(imageURL)
+    setPreviewURL(imageURL);
     // const imageData = new FormData();
     // imageData.append('image', files[0]);
     // imageData.append('file_name', files[0].name);
@@ -28,28 +26,28 @@ function InsertMedia() {
     // onImageUpload(imageData, imageURL, itemIndex);
   }
 
-  return <Styled.Root>
-    <Styled.Card>
-      <Styled.Icon src={uploadIcon} alt="upload"/>
-      <Styled.Input
-        type="file"
-        name="file"
-        id="file"
-        onChange={handleImageChange}
-        // name={name}
-        // componentName={componentName}
-      />
-      <label for="file">
-        <Styled.UploadBtn>
-          Choose Image
-        </Styled.UploadBtn>
-      </label>
-      <Styled.List>
-        <Styled.Item>Image should be 1024 x 1024 pixels</Styled.Item>
-        <Styled.Item>Acceptable file type: jpg, jpeg, png, svg</Styled.Item>
-      </Styled.List>
-    </Styled.Card>
-  </Styled.Root>;
+  return (
+    <Styled.Root>
+      <Styled.Card>
+        <Styled.Icon src={uploadIcon} alt="upload" />
+        <Styled.Input
+          type="file"
+          name="file"
+          id="file"
+          onChange={handleImageChange}
+          // name={name}
+          // componentName={componentName}
+        />
+        <label htmlFor="file">
+          <Styled.UploadBtn>Choose Image</Styled.UploadBtn>
+        </label>
+        <Styled.List>
+          <Styled.Item>Image should be 1024 x 1024 pixels</Styled.Item>
+          <Styled.Item>Acceptable file type: jpg, jpeg, png, svg</Styled.Item>
+        </Styled.List>
+      </Styled.Card>
+    </Styled.Root>
+  );
 }
 
 InsertMedia.propTypes = {};
