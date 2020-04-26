278d0c15acd76c8a715ae302f4f5a77977005874
Mohit Kumar <mohitkumar.7@outlook.com>
2020-04-27 02:42:29 +0530

    chore: style preview of uploaded image

--- app/components/Preview/index.js
+++ app/components/Preview/index.js
@@ -6,7 +6,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 import { isEmpty } from 'lodash';
 import Styled from './style';
 
@@ -14,7 +14,26 @@ import Styled from './style';
 function Preview({ previewURL }) {
 
   if(!isEmpty(previewURL)) {
-    return <img src={previewURL} />
+    return (
+      <div>
+        <Styled.Root>
+          <Styled.Card>
+            <a href={previewURL} target="_blank">
+              <Styled.OriginalImg src={previewURL} />
+            </a>
+            <Styled.CardContent>
+              <Styled.ImgInfo>
+                Original Image: 1024 x 1024
+              </Styled.ImgInfo>
+              <Styled.RemoveBtn to="/">
+                Change
+              </Styled.RemoveBtn>
+            </Styled.CardContent>
+          </Styled.Card>
+        </Styled.Root>
+      </div>
+    )
+
   }
   return (
     <Styled.NoPreview>
--- app/components/Preview/style.js
+++ app/components/Preview/style.js
@@ -1,4 +1,57 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+const Root = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+const Card = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  color: initial;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+  border-bottom-right-radius: 16px;
+  border-bottom-left-radius: 16px;
+  margin: 16px;
+  background-color: ${props => props.theme.base};
+`;
+
+const OriginalImg = styled.img`
+  max-width: 420px;
+`;
+
+const CardContent = styled.div`
+  padding: 16px;
+  margin-top: 12px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ImgInfo = styled.p`
+  margin: 0 0 12px;
+  color: gray;
+`
+
+const RemoveBtn = styled(NavLink)`
+  cursor: pointer;
+  border-radius: 24px;
+  padding: 12px 40px;
+  color: ${props => props.theme.base};
+  width: fit-content;
+  margin: 0 auto 12px auto;
+  font-size: 16px;
+  font-weight: bold;
+  text-decoration: none;
+  background-image: linear-gradient(
+    to right bottom,
+    #00c7d4,
+    #00bce5,
+    #00aef5,
+    #009dff,
+    #0088ff
+  );
+`;
 
 const NoPreview = styled.div`
   justify-content: center;
@@ -13,6 +66,12 @@ const NoPreviewMsg = styled.div`
 `;
 
 export default {
+  Card,
+  Root,
+  OriginalImg,
+  CardContent,
+  ImgInfo,
+  RemoveBtn,
   NoPreview,
   NoPreviewMsg,
 };
