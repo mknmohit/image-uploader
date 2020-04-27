29ca20b3bc417c4d0bdcb3e7ca02832622b2775e
Mohit Kumar <mohitkumar.7@outlook.com>
2020-04-27 06:09:18 +0530

    chore: crop image and create its canvas

--- app/components/Preview/index.js
+++ app/components/Preview/index.js
@@ -4,23 +4,92 @@
  *
  */
 
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 import { NavLink } from 'react-router-dom';
 import { isEmpty } from 'lodash';
+
 import Styled from './style';
 
-
 function Preview({ previewURL }) {
+
+  const [imgRef, setImgRef] = useState(null);
+  const [cropInfo, setCropInfo] = useState({
+    unit: 'px',
+    width: 755,
+    height: 450,
+  });
+  const [croppedUrl, setCroppedUrl] = useState();
+
+  const onImageLoaded = useCallback(img => {
+    setImgRef(img);
+  }, []);
+
+  const onCropChange = crop => setCropInfo(crop);
+
+  const onCropComplete = async crop => {
+    if (imgRef && crop.width && crop.height) {
+      createCropPreview(imgRef, crop, 'newFile.jpeg');
+    }
+  };
+
+  const createCropPreview = async (image, crop, fileName) => {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (!blob) {
+          reject(new Error('Canvas is empty'));
+          return;
+        }
+        blob.name = fileName;
+        window.URL.revokeObjectURL(croppedUrl);
+        setCroppedUrl(window.URL.createObjectURL(blob));
+      }, 'image/jpeg');
+    });
+  };
 
   if(!isEmpty(previewURL)) {
     return (
       <div>
         <Styled.Root>
           <Styled.Card>
-            <a href={previewURL} target="_blank">
+            {/* <a href={previewURL} target="_blank">
               <Styled.OriginalImg src={previewURL} />
-            </a>
+            </a> */}
+
+            <ReactCrop
+              src={previewURL}
+              crop={cropInfo}
+              minWidth={755}
+              maxWidth={755}
+              minHeight={450}
+              maxHeight={450}
+              ruleOfThirds
+              onImageLoaded={onImageLoaded}
+              onChange={onCropChange}
+              onComplete={onCropComplete}
+            />
+            
             <Styled.CardContent>
               <Styled.ImgInfo>
                 Original Image: 1024 x 1024
@@ -30,6 +99,7 @@ function Preview({ previewURL }) {
               </Styled.RemoveBtn>
             </Styled.CardContent>
           </Styled.Card>
+          {croppedUrl && <img alt="Crop preview" src={croppedUrl} />}
         </Styled.Root>
       </div>
     )
